43995c70dc4120851cc048927d3fa7f22de09b08
xiaoyuyu2000 <84629116+xiaoyuyu2000@users.noreply.github.com>
2023-04-15 13:55:02 +0800

    Create 2-image-asset.dart

---
+++ lib/2-image-asset.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Love u, RR'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Image.asset('assets/3.jpg'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        backgroundColor: Colors.green[700],
+        child: Text('CLICK'),
+      ),
+    );
+  }
+}
