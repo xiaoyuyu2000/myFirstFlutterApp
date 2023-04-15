c95586e7d36e6cfa73b8fb078db8641cfea17dc0
xiaoyuyu2000 <84629116+xiaoyuyu2000@users.noreply.github.com>
2023-04-15 13:56:50 +0800

    Update main.dart

--- 1-font-color/main.dart
+++ 1-font-color/main.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Love u, RR'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Text(
+          'RR love you.',
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        backgroundColor: Colors.green[700],
+        child: Text('CLICK'),
+      ),
+    );
+  }
+}
