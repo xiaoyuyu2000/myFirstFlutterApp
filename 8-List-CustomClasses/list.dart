d74b27f519d733c0d893b17bce2afb53398a823a
xiaoyuyu2000 <84629116+xiaoyuyu2000@users.noreply.github.com>
2023-04-17 16:39:33 +0800

    Create list.dart

---
+++ 8-List-CustomClasses/list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    'Don\'t let other people tell who you are,',
+    'Be yourself; everyone else is already taken.',
+    'The truth is rarely pure and never simple.'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList()),
+    );
+  }
+}
+
