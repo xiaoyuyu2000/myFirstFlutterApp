1e344b25c48d99ed784213e8e2fe09762ec10f93
xiaoyuyu2000 <84629116+xiaoyuyu2000@users.noreply.github.com>
2023-04-17 14:23:01 +0800

    Update 1.dart

--- 7-rr_id_project/1.dart
+++ 7-rr_id_project/1.dart
@@ -8,8 +8,12 @@ class RrCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green[900],
       appBar: AppBar(
         title: Text('RR ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        elevation: 0.0,
       ),
     );
   }
