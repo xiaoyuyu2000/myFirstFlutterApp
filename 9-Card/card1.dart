8f5187a4399d615ad33a9fb3f3651a59e5492573
xiaoyuyu2000 <84629116+xiaoyuyu2000@users.noreply.github.com>
2023-04-19 20:38:02 +0800

    Update card1.dart

--- 9-Card/card1.dart
+++ 9-Card/card1.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(const MaterialApp(
   home: MyCard(),
 ));
 
 class MyCard extends StatefulWidget {
+  const MyCard({super.key});
+
   @override
   State<MyCard> createState() => _MyCardState();
 }
@@ -13,32 +15,35 @@ class MyCard extends StatefulWidget {
 class _MyCardState extends State<MyCard> {
 
   List<Quote3> quotes = [
-    Quote3(text: 'Don\'t let other people tell who you are,', author: 'J.Tryhy'),
-    Quote3(text: 'Be yourself; everyone else is already taken.', author: 'A.Bryhy'),
-    Quote3(text: 'The truth is rarely pure and never simple.', author: 'C.Dryhy')
+    Quote3(text: 'Don\'t let other people tell who you are,', author: 'J.Trashy'),
+    Quote3(text: 'Be yourself; everyone else is already taken.', author: 'A.Brushy'),
+    Quote3(text: 'The truth is rarely pure and never simple.', author: 'C.Dryly')
   ];
 
   Widget quoteTemplate(quote) {
     return Card(
-      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      color: Colors.green[50],
       child: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Text(
-              quote.text,
+              '~ ${quote.text}',
               style: TextStyle(
                 fontSize: 18.0,
-                color: Colors.grey[600],
+                color: Colors.grey[700],
               ),
             ),
-            SizedBox(height: 8.0,),
+            const SizedBox(height: 8.0,),
             Text(
               quote.author,
+              textAlign: TextAlign.end,
               style: TextStyle(
                 fontSize: 14.0,
-                color: Colors.grey[800],
+                color: Colors.grey[900],
+                fontStyle: FontStyle.italic,
               ),
             ),
           ],
@@ -51,10 +56,11 @@ class _MyCardState extends State<MyCard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Awesome Cards'),
+        title: const Text('My Awesome Cards'),
         centerTitle: true,
         backgroundColor: Colors.green,
       ),
+      backgroundColor: Colors.green[200],
       body: Column(
         children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
