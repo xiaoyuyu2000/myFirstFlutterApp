9165d6ba477d43d9b2d125201063d2c1f9b2008f
xiaoyuyu2000 <84629116+xiaoyuyu2000@users.noreply.github.com>
2023-04-19 20:57:05 +0800

    Update card2.dart

--- 9-Card/card2.dart
+++ 9-Card/card2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(const MaterialApp(
   home: MyCard(),
@@ -20,9 +21,6 @@ class _MyCardState extends State<MyCard> {
     Quote3(text: 'The truth is rarely pure and never simple.', author: 'C.Dryly')
   ];
 
-  Widget quoteTemplate(quote) {
-    return QuoteCard(quote: quote);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,48 +32,10 @@ class _MyCardState extends State<MyCard> {
       ),
       backgroundColor: Colors.green[200],
       body: Column(
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
       ),
     );
   }
 }
 
-// Widget QuoteCard
-class QuoteCard extends StatelessWidget {
 
-  final Quote3 quote;
-  const QuoteCard({super.key, required this.quote});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-      color: Colors.green[50],
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              '~ ${quote.text}',
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[700],
-              ),
-            ),
-            const SizedBox(height: 8.0,),
-            Text(
-              quote.author,
-              textAlign: TextAlign.end,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[900],
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
