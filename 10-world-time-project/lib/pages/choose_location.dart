fc4129ea2257f462b7aaecb5649666d5399083e0
xiaoyuyu2000 <84629116+xiaoyuyu2000@users.noreply.github.com>
2023-04-21 16:31:02 +0800

    Create choose_location.dart

---
+++ 10-world-time-project/lib/pages/choose_location.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Text('choose location screen'),
+    );
+  }
+}
