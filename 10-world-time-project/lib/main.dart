9f1c7844160dc598594d7985db2eed829b457560
xiaoyuyu2000 <84629116+xiaoyuyu2000@users.noreply.github.com>
2023-04-23 20:46:10 +0800

    Add files via upload

--- 10-world-time-project/lib/main.dart
+++ 10-world-time-project/lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-
+import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 import 'package:world_time/pages/choose_location.dart';
-import 'package:world_time/pages/home.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/home',
+  initialRoute: '/',
   routes: {
-    '/': (context) => Loading(),
-    '/home': (context) => Home(),
-    '/location': (context) => ChooseLocation(),
+    '/': (context) => const Loading(),
+    '/home': (context) => const Home(),
+    '/location': (context) => const ChooseLocation(),
   },
 ));
